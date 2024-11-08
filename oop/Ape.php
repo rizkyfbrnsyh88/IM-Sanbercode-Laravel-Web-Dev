cf01fd9c47ee2ecd0188b53cb5033d35b4344628
rizkyfbrnsyh88 <rizkyfebriansyah88@gmail.com>
2024-11-08 07:46:02 +0700

    Tugas OOP Revisi2

--- oop/Ape.php
+++ oop/Ape.php
@@ -2,6 +2,7 @@
 require_once('animal.php');
 
 class Ape extends Animal{
+    public $legs = 2;
 
     public function yell(){
         return "Auooo";
--- oop/Frog.php
+++ oop/Frog.php
@@ -2,7 +2,6 @@
 require_once('animal.php');
 
 class Frog extends Animal{
-    public $legs = 2;
 
     public function jump(){
         return "hop hop";
